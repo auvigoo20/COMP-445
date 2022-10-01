c4ba8a577c09522ee0da6f52e2ddf6b04e1878b7
Auvigoo <auvigooa@gmail.com>
2022-10-01 16:42:42 -0400

    start command line arguments

--- Assignment1/httpc.py
+++ Assignment1/httpc.py
@@ -1,15 +1,16 @@
+import argparse
 import socket
 
 
 def testrun():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = "httpbin.org"
+    host = "google.ca"
 
     sock.connect((host, 80))
 
     # GET only
-    # line = "GET /status/418 HTTP/1.0\r\nHost:httpbin.org\r\n\r\n"
+    line = "GET /?q=hello+world HTTP/1.0\r\nHost:google.ca\r\n\r\n"
 
     # GET with query params
     # line = "GET /get?course=networking&assignment=1 HTTP/1.0\r\nHost:httpbin.org\r\n\r\n"
@@ -19,16 +20,16 @@ def testrun():
     # line = "POST /post HTTP/1.0\r\nHost:httpbin.org\r\nContent-Type:application/json\r\n" + "Content-Length:" + str(len(someJSON)) + "\r\n\r\n" + someJSON + "\r\n"
 
     # POST with header and file
-    file = open("hello.txt", "r")
-    try:
-        fileContent = file.read()
-        line = "POST /post HTTP/1.0\r\nHost:httpbin.org\r\nContent-Type:application/json\r\n" + "Content-Length:" + str(
-            len(fileContent)) + "\r\n\r\n" + fileContent + "\r\n"
-
-    except Exception as e:
-        print(f"An error has occured when reading the file:{e}")
-    finally:
-        file.close()
+    # file = open("hello.txt", "r")
+    # try:
+    #     fileContent = file.read()
+    #     line = "POST /post HTTP/1.0\r\nHost:httpbin.org\r\nContent-Type:application/json\r\n" + "Content-Length:" + str(
+    #         len(fileContent)) + "\r\n\r\n" + fileContent + "\r\n"
+    #
+    # except Exception as e:
+    #     print(f"An error has occured when reading the file:{e}")
+    # finally:
+    #     file.close()
 
 
 
@@ -42,4 +43,16 @@ def testrun():
     sock.close()
 
 
-testrun()
+# def run_http_client(method, verbose, headers, data, file, URL):
+
+# testrun()
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('--help', action='store_true')
+parser.add_argument('command', choices=['get', 'post', ''], nargs='?', default='')
+parser.add_argument("-v", action="store_true")
+parser.add_argument("-h", action="append", nargs="*")
+parser.add_argument('-d')
+parser.add_argument('-f')
+parser.add_argument("URL",  nargs='?')
+args = parser.parse_args()
+print(args)
