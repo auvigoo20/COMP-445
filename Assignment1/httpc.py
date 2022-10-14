f27371b51d3c2a8bd3fef8bec3cd3661907f1d12
Auvigoo <auvigooa@gmail.com>
2022-10-14 12:53:23 -0400

    kinda separate http library and curl implementation

--- Assignment1/httpc.py
+++ Assignment1/httpc.py
@@ -29,15 +29,15 @@ help_post = ("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] U
              "Either [-d] or [-f] can be used but not both.")
 
 
-def build_http_request(args):
-    parsed_URL = urlparse(args.URL)
+def build_http_request(url, command, headers, data, file):
+    parsed_URL = urlparse(url)
     host = parsed_URL.netloc
     path = parsed_URL.path if len(parsed_URL.path) > 0 else "/"
     query = parsed_URL.query
 
     request = ""
 
-    if args.command == 'get':
+    if command == 'get':
         # Build the GET request
         request = "GET " + path
 
@@ -49,12 +49,12 @@ def build_http_request(args):
         request = request + " HTTP/1.0\r\nHost:" + host + "\r\n" + "User-Agent:Concordia-HTTP/1.0\r\n"
 
         # Append headers
-        if args.headers:
-            for header in args.headers:
+        if headers:
+            for header in headers:
                 request = request + header + "\r\n"
 
         request = request + "\r\n"
-    elif args.command == 'post':
+    elif command == 'post':
         # Build the POST request
         request = "POST " + path
 
@@ -66,17 +66,17 @@ def build_http_request(args):
         request = request + " HTTP/1.0\r\nHost:" + host + "\r\n" + "User-Agent:Concordia-HTTP/1.0\r\n"
 
         # Append headers
-        if args.headers:
-            for header in args.headers:
+        if headers:
+            for header in headers:
                 request = request + header + "\r\n"
 
         # Append inline data
-        if args.data:
-            request = request + "Content-Length:" + str(len(args.data)) + "\r\n\r\n" + args.data
+        if data:
+            request = request + "Content-Length:" + str(len(data)) + "\r\n\r\n" + data
 
         # Append file data
-        if args.file:
-            file = open(args.file, "r")
+        if file:
+            file = open(file, "r")
             try:
                 file_content = file.read()
                 request = request + "Content-Length:" + str(len(file_content)) + "\r\n\r\n" + file_content
@@ -118,7 +118,7 @@ def run_http_client(args):
     else:
         parsed_URL = urlparse(args.URL)
         host = parsed_URL.netloc
-        request = build_http_request(args)
+        request = build_http_request(args.URL, args.command, args.headers, args.data, args.file)
         response = send_request(host, request)
 
         # REDIRECTION HANDLING (response codes 3xx)
@@ -141,7 +141,7 @@ def run_http_client(args):
                     redirect_url = parsed_URL.scheme + "://" + host + redirect_url
                 args.URL = redirect_url
                 # Send new request
-                request = build_http_request(args)
+                request = build_http_request(args.URL, args.command, args.headers, args.data, args.file)
                 response = send_request(host, request)
 
                 # Check the status code of the newly sent request. If it is 3xx, repeat the process again
