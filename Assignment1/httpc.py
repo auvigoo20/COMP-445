c7d4b92b0e965f90b0a84796b7d134c5383ea4ac
Auvigoo <auvigooa@gmail.com>
2022-10-14 16:58:51 -0400

    separate http library and curl implementation in different files

--- Assignment1/httpc.py
+++ Assignment1/httpc.py
@@ -1,6 +1,6 @@
 import argparse
-import socket
 from urllib.parse import urlparse
+from httpc_library import build_http_request, send_request
 
 help_general = ("httpc is a curl-like application but supports HTTP protocol only.\n"
                 "Usage:\n"
@@ -27,83 +27,6 @@ help_post = ("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] U
              "\t-r Allows redirection to occur when responses have status codes 3xx.\n"
              "\t-o output-file Write the console output to an external file.\n"
              "Either [-d] or [-f] can be used but not both.")
-
-
-def build_http_request(url, command, headers, data, file):
-    parsed_URL = urlparse(url)
-    host = parsed_URL.netloc
-    path = parsed_URL.path if len(parsed_URL.path) > 0 else "/"
-    query = parsed_URL.query
-
-    request = ""
-
-    if command == 'get':
-        # Build the GET request
-        request = "GET " + path
-
-        # Append query params
-        if len(query) > 0:
-            request = request + "?" + query
-
-        # Append host
-        request = request + " HTTP/1.0\r\nHost:" + host + "\r\n" + "User-Agent:Concordia-HTTP/1.0\r\n"
-
-        # Append headers
-        if headers:
-            for header in headers:
-                request = request + header + "\r\n"
-
-        request = request + "\r\n"
-    elif command == 'post':
-        # Build the POST request
-        request = "POST " + path
-
-        # Append query params
-        if len(query) > 0:
-            request = request + "?" + query
-
-        # Append host and user agent
-        request = request + " HTTP/1.0\r\nHost:" + host + "\r\n" + "User-Agent:Concordia-HTTP/1.0\r\n"
-
-        # Append headers
-        if headers:
-            for header in headers:
-                request = request + header + "\r\n"
-
-        # Append inline data
-        if data:
-            request = request + "Content-Length:" + str(len(data)) + "\r\n\r\n" + data
-
-        # Append file data
-        if file:
-            file = open(file, "r")
-            try:
-                file_content = file.read()
-                request = request + "Content-Length:" + str(len(file_content)) + "\r\n\r\n" + file_content
-            except Exception as e:
-                print(f"An error has occured when reading the file:{e}")
-            finally:
-                file.close()
-
-        request = request + "\r\n"
-
-    return request
-
-
-def send_request(host, request):
-    # Connect to host
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, 80))
-
-    # Encode and send request
-    request_encoded = request.encode("utf-8")
-    sock.send(request_encoded)
-
-    # Receive and decode response
-    response_encoded = sock.recv(1024)
-    response = response_encoded.decode("utf-8")
-    sock.close()
-    return response
 
 
 def run_http_client(args):
