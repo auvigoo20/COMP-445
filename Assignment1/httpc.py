acdb5ab18c3ec55937a8a8fc02472dee190cbb8e
Auvigoo <auvigooa@gmail.com>
2022-11-11 23:57:41 -0500

    add check in httpc

--- Assignment1/httpc.py
+++ Assignment1/httpc.py
@@ -41,7 +41,7 @@ def run_http_client(args):
     else:
         parsed_URL = urlparse(args.URL)
         host = parsed_URL.netloc
-        if len(host) < 1:
+        if len(host) < 1 or "localhost" in host:
             host = "localhost"
         request = build_http_request(args.URL, args.command, args.headers, args.data, args.file)
         response = send_request(host, request, 8080)
@@ -67,7 +67,7 @@ def run_http_client(args):
                 args.URL = redirect_url
                 # Send new request
                 request = build_http_request(args.URL, args.command, args.headers, args.data, args.file)
-                response = send_request(host, request, 8007)
+                response = send_request(host, request, 8080)
 
                 # Check the status code of the newly sent request. If it is 3xx, repeat the process again
                 response_details = response.split("\r\n\r\n")[0]
