ce41bf38a79f687c74d0fd49954ce8a9c52000f1
Auvigoo <auvigooa@gmail.com>
2022-11-15 19:52:23 -0500

    add help command line option

--- Assignment2/httpfs.py
+++ Assignment2/httpfs.py
@@ -6,8 +6,17 @@ from httpfs_library import build_response_from_request
 
 current_directory = os.getcwd()
 
+help_message = ("httpfs is a simple file server.\n"
+                "usage: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]\n"
+                "-v Prints debugging messages.\n"
+                "-p Specifies the port number that the server will listen and serve at. Default is 8080.\n"
+                "-d Specifies the directory that the server will use to read/write requested files. Default is the "
+                "current directory when launching the application.\n")
+
 
 def run_server(host, port):
+    if args.help:
+        print(help_message)
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         listener.bind((host, port))
